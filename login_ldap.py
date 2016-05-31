b1519c7defeab4d17304d5423b24e18a0139c738
Nabil J. Marquez <nabiljesus@gmail.com>
2016-05-31 02:39:24 -0430

    Ldap login

---
+++ login_ldap.py
@@ -0,0 +1,26 @@
+#ble to import ldap run pip install python-ldap 
+
+import ldap
+ 
+if __name__ == "__main__":
+  	ldap_server="ldap.ldc.usb.ve"
+	username = "nabil"
+	password= "potato"
+	# the following is the user_dn format provided by the ldap server
+	user_dn = "uid="+username+",ou=People,dc=ldc,dc=usb,dc=ve"
+	# adjust this to your base dn for searching
+	base_dn = "dc=ldc,dc=usb,dc=ve"
+	connect = ldap.open(ldap_server)
+	search_filter = "uid="+username
+	try:
+		#if authentication successful, get the full user data
+		connect.bind_s(user_dn,password)
+		result = connect.search_s(base_dn,ldap.SCOPE_SUBTREE,search_filter)
+		print(result)
+		# return all user data results
+		connect.unbind_s()
+		print result
+	except ldap.LDAPError:
+		connect.unbind_s()
+		print('fallo')
+
